2544e5058e53eea2eefca0c75570c10bd6665485
DimasX011 <Dimon1998daf@mail.ru>
2021-12-08 10:15:37 +0600

    start

---
+++ Project1/IManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Project1
+{
+    internal interface IManager
+    {
+        void Create(string name);
+
+        List<string> ShowCatalog(string nameCatalog);
+
+        void Delete(string name);
+
+        void ReName(string name);
+
+        void Copy(string name);
+
+        void CalculateFile(string name);
+
+        void Search(string name);
+
+        DataFile Datafile(string filename);
+
+
+
+
+
+    }
+}
---
+++ Project1/Realize.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Project1
+{
+    class Realize : IManager
+    {
+        public void CalculateFile(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Copy(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Create(string name)
+        {
+            string path = @"C:\Users\Dimon\source\repos\для курсача\";
+            FileStream fstream = new FileStream(path + name, FileMode.OpenOrCreate);
+            fstream.Close();
+        }
+
+        public DataFile Datafile(string filename)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ReName(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Search(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<string> ShowCatalog(string nameCatalog)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
